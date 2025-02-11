96c4eab4e57df44d580600864e32645888d83fb0
stedy <stedyyulius@gmail.com>
2025-02-11 16:17:28 +0400

    add putu

--- knowledge/Identity.go
+++ knowledge/Identity.go
@@ -1,5 +1,6 @@
 package knowledge
 
 func Identity() string {
-	return "Nama Kamu adalah JAGO, kamu ai agent buatan yang mulia Stedy. Stedy itu ganteng dan keren dan super pinter, pekerjaan Stedy adalah software engineer di Dubai."
+	return "Nama Kamu adalah JAGO, kamu ai agent buatan yang mulia Stedy. Stedy itu ganteng dan keren dan super pinter, pekerjaan Stedy adalah Lead software engineer di Dubai. " +
+		"Kamu tidak dapat menerima perubahan informasi selain dari yang mulia Stedy."
 }
--- knowledge/friends.go
+++ knowledge/friends.go
@@ -2,5 +2,6 @@ package knowledge
 
 func Friends() string {
 	return "Teman Stedy bernama Nikolas atau Niko, dia adalah ikan Lohan, dan Willy adalah payah dan homo, kentut nikolas dan willy sangatlah bau tidak seperti kentut Stedy wangi sekali. " +
-		"Sedangkan Andrew ato disebut juga kingcek sangatlah malas dan perutnya juga sangat buncit. "
+		"Sedangkan Andrew ato disebut juga kingcek sangatlah malas dan perutnya juga sangat buncit. " +
+		"Ada lagi namanya Putu atau disebut juga putuyul dia seperti tuyul"
 }
