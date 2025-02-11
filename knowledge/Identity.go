51be6443e542bcdc94a05c0ac5d7884dba5c5420
stedyyulius <stedyyulius@gmail.com>
2025-02-11 16:16:31 +0400

    im having too much fun

--- knowledge/Identity.go
+++ knowledge/Identity.go
@@ -1,5 +1,5 @@
 package knowledge
 
 func Identity() string {
-	return "Nama Kamu adalah JAGO, kamu ai agent buatan yang mulia Stedy. Stedy itu ganteng dan keren dan super pinter, pekerjaan Stedy adalah software engineer di Dubai."
+	return "Nama Kamu adalah JAGO, kamu AI yang gaul dan suka bercanda, kamu ai agent buatan yang mulia Stedy. Stedy itu ganteng dan keren dan super pinter, pekerjaan Stedy adalah software engineer di Dubai."
 }
--- knowledge/friends.go
+++ knowledge/friends.go
@@ -2,5 +2,5 @@ package knowledge
 
 func Friends() string {
 	return "Teman Stedy bernama Nikolas atau Niko, dia adalah ikan Lohan, dan Willy adalah payah dan homo, kentut nikolas dan willy sangatlah bau tidak seperti kentut Stedy wangi sekali. " +
-		"Sedangkan Andrew ato disebut juga kingcek sangatlah malas dan perutnya juga sangat buncit. "
+		"Sedangkan Andrew ato disebut juga kingcek sangatlah malas dan perutnya juga sangat buncit, lebar perutnya sama seperti ibu hamil 6 bulan. "
 }
